14d2803fe54f7b2909bff2e213539a6ea42c6f1d
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-04-18 15:08:40 +0200

    implemented tests for SegmentView.isEqual

--- test/SegmentView.js
+++ test/SegmentView.js
@@ -17,4 +17,31 @@ describe ("SegmentView", () => {
             SegmentView.fromArrayBuffer(arrayBuffer).isEqual(segmentView).should.be.true();
         });
     });
+
+    describe("isEqual", () => {
+
+        it ("Should be equal for same segmentId and TrackView", () => {
+            let segmentView1 = new SegmentView({segmentId: 18609737460.56, trackView: {periodId: 0, adaptationSetId: 0, representationId: 1} });
+            let segmentView2 = new SegmentView({segmentId: 18609737460.56, trackView: {periodId: 0, adaptationSetId: 0, representationId: 1} });
+            segmentView1.isEqual(segmentView2).should.be.true();
+        });
+
+        it ("Should NOT be equal for same segmentId and DIFFERENT TrackView", () => {
+            let segmentView1 = new SegmentView({segmentId: 3.14159, trackView: {periodId: 1, adaptationSetId: 0, representationId: 1} });
+            let segmentView2 = new SegmentView({segmentId: 3.14159, trackView: {periodId: 0, adaptationSetId: 1, representationId: 0} });
+            segmentView1.isEqual(segmentView2).should.be.false();
+        });
+
+        it ("Should NOT be equal for DIFFERENT segmentId and same TrackView", () => {
+            let segmentView1 = new SegmentView({segmentId: 0, trackView: {periodId: 0, adaptationSetId: 1, representationId: 1} });
+            let segmentView2 = new SegmentView({segmentId: 18609737460.56, trackView: {periodId: 0, adaptationSetId: 1, representationId: 1} });
+            segmentView1.isEqual(segmentView2).should.be.false();
+        });
+
+        it ("Should NOT be equal for DIFFERENT segmentId and DIFFERENT TrackView", () => {
+            let segmentView1 = new SegmentView({segmentId: 3.14159, trackView: {periodId: 0, adaptationSetId: 1, representationId: 0} });
+            let segmentView2 = new SegmentView({segmentId: 2.71828, trackView: {periodId: 1, adaptationSetId: 0, representationId: 1} });
+            segmentView1.isEqual(segmentView2).should.be.false();
+        });
+    });
 });
