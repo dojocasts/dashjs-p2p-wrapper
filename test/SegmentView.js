b3f7d00fd0609ea2cea99cbbb4f0e568244b25b9
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-06-10 11:55:12 +0200

    [#120756719] code formatting fixes

--- test/SegmentView.js
+++ test/SegmentView.js
@@ -2,7 +2,7 @@ var should = require("should");
 var SegmentView = require("../lib/SegmentView");
 var TrackView = require("../lib/TrackView");
 
-describe ("SegmentView", () => {
+describe("SegmentView", () => {
     describe("toArrayBuffer", () => {
 
         it("Should be equal to its duplicate if timeStamp < 2^32", () => {
@@ -20,25 +20,25 @@ describe ("SegmentView", () => {
 
     describe("isEqual", () => {
 
-        it ("Should be equal for same timeStamp and TrackView", () => {
+        it("Should be equal for same timeStamp and TrackView", () => {
             let segmentView1 = new SegmentView({timeStamp: 18609737460.56, trackView: {periodId: 0, adaptationSetId: 0, representationId: 1} });
             let segmentView2 = new SegmentView({timeStamp: 18609737460.56, trackView: {periodId: 0, adaptationSetId: 0, representationId: 1} });
             segmentView1.isEqual(segmentView2).should.be.true();
         });
 
-        it ("Should NOT be equal for same timeStamp and DIFFERENT TrackView", () => {
+        it("Should NOT be equal for same timeStamp and DIFFERENT TrackView", () => {
             let segmentView1 = new SegmentView({timeStamp: 3.14159, trackView: {periodId: 1, adaptationSetId: 0, representationId: 1} });
             let segmentView2 = new SegmentView({timeStamp: 3.14159, trackView: {periodId: 0, adaptationSetId: 1, representationId: 0} });
             segmentView1.isEqual(segmentView2).should.be.false();
         });
 
-        it ("Should NOT be equal for DIFFERENT timeStamp and same TrackView", () => {
+        it("Should NOT be equal for DIFFERENT timeStamp and same TrackView", () => {
             let segmentView1 = new SegmentView({timeStamp: 0, trackView: {periodId: 0, adaptationSetId: 1, representationId: 1} });
             let segmentView2 = new SegmentView({timeStamp: 18609737460.56, trackView: {periodId: 0, adaptationSetId: 1, representationId: 1} });
             segmentView1.isEqual(segmentView2).should.be.false();
         });
 
-        it ("Should NOT be equal for DIFFERENT timeStamp and DIFFERENT TrackView", () => {
+        it("Should NOT be equal for DIFFERENT timeStamp and DIFFERENT TrackView", () => {
             let segmentView1 = new SegmentView({timeStamp: 3.14159, trackView: {periodId: 0, adaptationSetId: 1, representationId: 0} });
             let segmentView2 = new SegmentView({timeStamp: 2.71828, trackView: {periodId: 1, adaptationSetId: 0, representationId: 1} });
             segmentView1.isEqual(segmentView2).should.be.false();
@@ -47,28 +47,28 @@ describe ("SegmentView", () => {
 
     describe("isInTrack", () => {
 
-        it ("Should be in track", () => {
+        it("Should be in track", () => {
             let trackView = new TrackView({periodId: 0, adaptationSetId: 0, representationId: 0});
             let segmentView = new SegmentView({ timeStamp: 1.61803, trackView: {periodId: 0, adaptationSetId: 0, representationId: 0} });
 
             segmentView.isInTrack(trackView).should.be.true();
         });
 
-        it ("Should NOT be in track if periodId is DIFFERENT", () => {
+        it("Should NOT be in track if periodId is DIFFERENT", () => {
             let trackView = new TrackView({periodId: 1, adaptationSetId: 0, representationId: 0});
             let segmentView = new SegmentView({ timeStamp: 1.61803, trackView: {periodId: 0, adaptationSetId: 0, representationId: 0} });
 
             segmentView.isInTrack(trackView).should.be.false();
         });
 
-        it ("Should NOT be in track if adaptationSetId is DIFFERENT", () => {
+        it("Should NOT be in track if adaptationSetId is DIFFERENT", () => {
             let trackView = new TrackView({periodId: 0, adaptationSetId: 1, representationId: 0});
             let segmentView = new SegmentView({ timeStamp: 1.61803, trackView: {periodId: 0, adaptationSetId: 0, representationId: 0} });
 
             segmentView.isInTrack(trackView).should.be.false();
         });
 
-        it ("Should NOT be in track if representationId is DIFFERENT", () => {
+        it("Should NOT be in track if representationId is DIFFERENT", () => {
             let trackView = new TrackView({periodId: 0, adaptationSetId: 0, representationId: 1});
             let segmentView = new SegmentView({ timeStamp: 1.61803, trackView: {periodId: 0, adaptationSetId: 0, representationId: 0} });
 
