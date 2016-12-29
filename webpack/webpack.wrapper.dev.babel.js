926849ff6b1dd48d85bffaef5e0e96cb99e1e5f9
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-12-29 12:52:09 +0300

    introduced webpack

---
+++ webpack/webpack.wrapper.dev.babel.js
@@ -0,0 +1,23 @@
+import webpack from 'webpack';
+import { baseConfig, ROOT_PATH } from './webpack.base';
+
+const { version } = require(`${ROOT_PATH}/package.json`);
+
+export default Object.assign(baseConfig, {
+    entry: [`${ROOT_PATH}/lib/DashjsWrapper.js`],
+
+    output: {
+        library: [
+            'DashjsWrapper'
+        ],
+        libraryTarget: 'umd',
+        path: `${ROOT_PATH}/dist/wrapper`,
+        filename: 'dashjs-p2p-wrapper.js',
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            _VERSION_: JSON.stringify(version),
+        }),
+    ]
+});
