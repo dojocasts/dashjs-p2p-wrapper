82bbf568360b8e8dd3abb033cb3e14806677d57d
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2017-03-13 15:22:47 +0100

    fixed tests

--- mochahook.js
+++ mochahook.js
@@ -3,3 +3,13 @@ require('babel-core/register')({
     ignore: /node_modules/,
 });
 window = global;
+
+global.dashjs = {
+    MediaPlayer: {
+        events: {
+            QUALITY_CHANGE_START: 'QUALITY_CHANGE_START',
+            STREAM_INITIALIZED: 'STREAM_INITIALIZED',
+            FRAGMENT_LOADING_ABANDONED: 'FRAGMENT_LOADING_ABANDONED',
+        },
+    },
+};
